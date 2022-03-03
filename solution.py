f71d5d25cda0f1e7453378507ef75f3f82b4ece9
James Petran <jm.ptrn@gmail.com>
2022-03-03 15:57:48 -0600

    alerts are alerting, quadkey calculation is working

--- solution.py
+++ solution.py
@@ -15,7 +15,9 @@ def main():
     map_zoom = 12
 
     def issue_alert(owner, name, quad_key):
+        print("in issue_alert")
         if quad_key in alerted_asset.keys():
+            print("alert in" + owner + ":" + name)
             return
 
         
@@ -37,15 +39,22 @@ def main():
             # if lightning_strike is a heartbeat, then the alert is irrelevant and does not need to be posted
             # and the for loop should go onto the next value of lightning_strike
             if lightning_strike['flashType'] == 9:
-                break
+                # print("heartbeat")
+                continue
             # assign lat, long to x, y
             x, y = lightning_strike['latitude'], lightning_strike['longitude']
             # calculate quadkey for given lat/long
             lightning_qk = quadkey.from_geo((x, y), map_zoom)
+            # print("lightning:")
+            # print(str(lightning_qk), type(str(lightning_qk)))
+            # print("asset:")
+            # print(asset_list[0]['quadKey'], type(asset_list[0]['quadKey']))
+            # print(asset_list[0])
             # iterate through asset list to compare quadkeys
             for asset in asset_list:
-                if (asset['quadKey'] == lightning_qk):
-                    issue_alert(asset["assetOwner"], asset["assetName"], lightning_qk)
+                if (asset['quadKey'] == str(lightning_qk)):
+                    owner, name = asset["assetOwner"], asset["assetName"]
+                    print("alert in" + owner + ":" + name)
 
 
 main()
