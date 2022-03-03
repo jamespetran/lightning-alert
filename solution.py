91bd416aaab3754c96f1af155c6591138d874c56
James Petran <jm.ptrn@gmail.com>
2022-03-02 22:53:38 -0600

    accessed receivedTime in each dict in the lightning_list list

--- solution.py
+++ solution.py
@@ -1,14 +1,25 @@
 import json
 
 def main():
+    # init lightning_list as empty list type
     lightning_list = []
 
+    # opens the file
     with open ('./data/lightning.json') as f:
+        # for each line in file f:
         for json_obj in f:
+            # lightning strike is 
             lightning_strike = json.loads(json_obj)
-            lightning_list.append(lightning_strike);
+            lightning_list.append(lightning_strike)
     
-    print(lightning_list)
+    # print(lightning_list[0])
+    for lightning_strike in lightning_list:
+        print(lightning_strike['receivedTime'])
+
+    # location_list = []
+
+    # location_list = json.load('./data/assets.json')
+    # print(location_list)
 
 
 main()
