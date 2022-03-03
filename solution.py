a6c8bc7aa4f04f0e0e06f4a6f7666ce76e45e8bf
James Petran <jm.ptrn@gmail.com>
2022-03-02 22:36:59 -0600

    ingest lightning strike data

--- solution.py
+++ solution.py
@@ -1,4 +1,14 @@
+import json
+
 def main():
-    print('test123')
+    lightning_list = []
+
+    with open ('./data/lightning.json') as f:
+        for json_obj in f:
+            lightning_strike = json.loads(json_obj)
+            lightning_list.append(lightning_strike);
+    
+    print(lightning_list)
+
 
 main()
