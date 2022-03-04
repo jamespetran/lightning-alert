041d0c4de952605bee15dda706ddbe908fd6742b
James Petran <jm.ptrn@gmail.com>
2022-03-03 21:40:17 -0600

    first full test of app, and fixed accidentally changed asset data...accidentally renamed one assetName to assetOwner...fixed now

--- solution.py
+++ solution.py
@@ -12,49 +12,50 @@ def main():
     # init alerted dictionary
     alerted_asset = {}
 
+    # supplied map zoom level in instructions (12) to generate quadKey from lat, long
     map_zoom = 12
 
-    def issue_alert(owner, name, quad_key):
-        print("in issue_alert")
-        if quad_key in alerted_asset.keys():
-            print("alert in" + owner + ":" + name)
-            return
-
-        
-
-    # assign json file of list of assets to location_list
+    # assign json file of list of assets to asset_list
     with open('./data/assets.json') as file:
         asset_list = json.load(file)
         for asset in asset_list:
             # print(asset["quadKey"])
             pass
-        # convert list to dict, with key = quadKey and value = `${assetOwner}:${assetName}` to reduce time complexity from O(n^2) to O(n)
-        # for asset in asset_list:
+        # convert list to dict, with key == quadKey and value == `${assetOwner}:${assetName}` to reduce time 
+        # complexity from O(n^2) to O(n + m) where n == lightning list and m == asset list => simplifies to O(n)
+        # for asset in asset_list: 
 
-    # opens the file
+    # opens the lightning strike file as f
     with open('./data/lightning.json') as f:
         # for each separate line in file f:
         for json_obj in f:
+            # assign this json_obj to new lightning strike
             lightning_strike = json.loads(json_obj)
-            # if lightning_strike is a heartbeat, then the alert is irrelevant and does not need to be posted
-            # and the for loop should go onto the next value of lightning_strike
+            # if lightning_strike is a heartbeat, then the alert is irrelevant and does not need to be displayed
+            # and the for loop should go onto the next lightning_strike in f
             if lightning_strike['flashType'] == 9:
-                # print("heartbeat")
                 continue
             # assign lat, long to x, y
             x, y = lightning_strike['latitude'], lightning_strike['longitude']
-            # calculate quadkey for given lat/long
+            # calculate lightning quadkey from lightning strike lat/long using pyquadkey2's quadkey.from_geo()
             lightning_qk = quadkey.from_geo((x, y), map_zoom)
-            # print("lightning:")
-            # print(str(lightning_qk), type(str(lightning_qk)))
-            # print("asset:")
-            # print(asset_list[0]['quadKey'], type(asset_list[0]['quadKey']))
-            # print(asset_list[0])
-            # iterate through asset list to compare quadkeys
+            
+            # loop through asset list to compare quadkeys to check if a match
             for asset in asset_list:
+                # check the asset given quadkey compared to the lightning strike 
+                # calculated quadkey from lat/long
                 if (asset['quadKey'] == str(lightning_qk)):
-                    owner, name = asset["assetOwner"], asset["assetName"]
-                    print("alert in" + owner + ":" + name)
+                    # assign to easier to use vars
+                    owner, name, location = asset["assetOwner"], asset["assetName"], asset['quadKey']
+                    # check if this quadkey has had a prev alert
+                    if location in alerted_asset:
+                        # then skip issuing an alert and continue onto the next asset in asset_list
+                        continue
+                    else:
+                        # add this quadkey to the alerted_asset dictionary
+                        alerted_asset[location] = str(owner) + " : " + str(name)
+                    # alerted_asset[str]
+                    print("alert in " + owner + ":" + name)
 
 
 main()
